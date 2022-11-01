d116adcb5eb06b7268b76cd9c8668f606a2af737
Javiera Varela <javiera.varela@usach.cl>
2022-10-31 22:32:37 -0300

    Otros  avances.
    Corrigiendo predicados malos :)

--- Laboratorio2.pl
+++ Laboratorio2.pl
@@ -1,5 +1,4 @@
-
-%TD1: Constructor1
+%2 - TD1: Constructor1
 imagen(Altura,Anchura,Pix,Imagen):-
     integer(Altura),
     integer(Anchura),
@@ -59,7 +58,7 @@ isHex(Pix) :-
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - is bitmap / imageIsBitmap: boolean
+%3 - TDA image - is bitmap / imageIsBitmap: boolean
 %Las funciones se llaman predicados!!!
 %Aqui se comprueba que los añadidos sean pixbits
 
@@ -84,21 +83,7 @@ isBitmap(Imagen) :-
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - is hexmap / imageIsHexmap: boolean
-
-% Recursión
-isHexmapAux([]) :- !.
-isHexmapAux([H|T]) :-
-    isHex(H),
-    isHexmapAux(T).
-
-isHexmap(Imagen) :-
-    getbyIndex(2, Imagen, Hexs),
-    isHexmapAux(Hexs).
-
-%------------------------------------------------------------------------------------------------------------------------------
-
-%TDA image - is rgbmap / imageIsPixmap: boolean
+%4 - TDA image - is rgbmap / imageIsPixmap: boolean
 
 % Recursión
 isRGBmapAux([]) :- !.
@@ -112,7 +97,21 @@ isRGBmap(Imagen) :-
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - flipH / imageFlipH:
+%5 - TDA image - is hexmap / imageIsHexmap: boolean
+
+% Recursión
+isHexmapAux([]) :- !.
+isHexmapAux([H|T]) :-
+    isHex(H),
+    isHexmapAux(T).
+
+isHexmap(Imagen) :-
+    getbyIndex(2, Imagen, Hexs),
+    isHexmapAux(Hexs).
+
+%------------------------------------------------------------------------------------------------------------------------------
+
+%6 - TDA image - flipH / imageFlipH:
 
 % Descripción: Predicado que invierte el orden de una lista.
 % Dominio: Lista X Lista X Lista
@@ -140,7 +139,7 @@ flipH(Imagen, ImagenH):-
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - flipV / imageFlipV:
+%7 - TDA image - flipV / imageFlipV:
 
 % (Altura - 1) = z
 % z - y = Nueva cordenada
@@ -161,7 +160,7 @@ flipV(Imagen, ImagenV):-
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - Crop / imageCrop:
+%8 - TDA image - Crop / imageCrop:
 
 % Descripción: Predicado que recorta una imagen en base a las coordenadas de un pixel.
 % Dominio: Imagen X Imagen X Entero X Entero X Entero X Entero
@@ -180,59 +179,123 @@ cropAux([H|T], X, Y, W, H, NT) :-
     getbyIndex(1, H, Y),
     cropAux(T, X, Y, W, H, NT).
 
-imageCrop(Imagen, ImagenC, X, Y, W, H) :-
-    getbyIndex(2, Imagen, Pixs),
+imageCrop(Imagen, X, Y, W, H, ImagenC) :-
+    getbyIndex(2, Imagen, Pixs), 
     cropAux(Pixs, X, Y, W, H, PixsC),
     ImagenC = [H,W,PixsC].
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - Rotate / imageRotate:
+%9 - TDA image - Rotate / imageRotate90:
 
 % Descripción: Predicado que rota una imagen en base a un ángulo.
-% Dominio: Imagen X Imagen X Entero
+% Dominio: Imagen X Imagen
 % Imagen: Imagen a rotar. Imagen: Imagen rotada. Entero: Ángulo de rotación.
 
-% Recursión
-rotateAux([], _, []) :- !.
-rotateAux([H|T], Angulo, [H|NT]) :-
-    getbyIndex(0, H, X),
-    getbyIndex(1, H, Y),
-    rotateAux(T, Angulo, NT).
+% Rotación en 90° es (x,y) = (-y,x).
 
-rotateAux([H|T], Angulo, NT) :-
-    getbyIndex(0, H, X),
-    getbyIndex(1, H, Y),
-    rotateAux(T, Angulo, NT).
-
-imageRotate(Imagen, ImagenR, Angulo) :-
+rotate90(Imagen, ImagenR) :-
     getbyIndex(2, Imagen, Pixs),
-    rotateAux(Pixs, Angulo, PixsR),
+    getbyIndex(0, Pixs, OldX),
+    getbyIndex(1, Pixs, OldY),
+    NewX is -OldY,
+    NewY is OldX,
+    replace(OldX, NewX, Pixs, NewPixs),
+    replace(OldY, NewY, NewPixs, PixsR),
     ImagenR = [Altura,Anchura,PixsR].
 
+% Se añaade la rotación en 180° y 270° para agregar predicados de mayor complejidad. Estos no son necesarios para el funcionamiento 
+% del programa, pero se agregan para tener una mayor variedad de predicados.
+% Rotación en 180° es (x,y) = (-x,-y).
+
+rotate180(Imagen, ImagenR) :-
+    getbyIndex(2, Imagen, Pixs),
+    getbyIndex(0, Pixs, OldX),
+    getbyIndex(1, Pixs, OldY),
+    NewX is -OldX,
+    NewY is -OldY,
+    replace(OldX, NewX, Pixs, NewPixs),
+    replace(OldY, NewY, NewPixs, PixsR),
+    ImagenR = [Altura,Anchura,PixsR].
+
+% Rotación en 270° es (x,y) = (y,-x).
+
+rotate270(Imagen, ImagenR) :-
+    getbyIndex(2, Imagen, Pixs),
+    getbyIndex(0, Pixs, OldX),
+    getbyIndex(1, Pixs, OldY),
+    NewX is OldY,
+    NewY is -OldX,
+    replace(OldX, NewX, Pixs, NewPixs),
+    replace(OldY, NewY, NewPixs, PixsR),
+    ImagenR = [Altura,Anchura,PixsR].
 
 %------------------------------------------------------------------------------------------------------------------------------
 
-%TDA image - imgRGBtoHex / imageRGBtoHex:
+%10 - TDA image - imgRGBtoHex / imageRGBtoHex:
 
 % Descripción: Predicado que convierte una imagen RGB a Hex.
 % Dominio: Imagen X Imagen
 % Imagen: Imagen RGB. Imagen: Imagen Hex.
 
+% Debemos comprender que el formato RGB es un formato de 3 bytes, mientras que el formato Hex es un formato de 6 bytes.
+% Eje: RGB = [255,255,255] -> Hex = [FFFFFF]
+% para esto, se debe dividir el valor de cada byte en 16, del cual se obtendrá el primer dígito del byte Hex. Si la división no es
+% exacta, el decimal sobrante se multiplicara por 16.
+% Eje: 255 / 16 = 15.9375
+% 15 = F, 
+% 0.9375 * 16 = 15 
+% 15 = F
+% Hex = [FF]
+
+% Se creara los casos bases para un predicado que permita tranformar un decimal a Hex.
+
 % Recursión
-RGBtoHexAux([], []) :- !.
-RGBtoHexAux([H|T], [H|NT]) :-
-    isPixRGB(H),
-    RGBtoHexAux(T, NT).
+byteToHex(0, 0) :- !.
+byteToHex(1, 1) :- !.
+byteToHex(2, 2) :- !.
+byteToHex(3, 3) :- !.
+byteToHex(4, 4) :- !.
+byteToHex(5, 5) :- !.
+byteToHex(6, 6) :- !.
+byteToHex(7, 7) :- !.
+byteToHex(8, 8) :- !.
+byteToHex(9, 9) :- !.
+byteToHex(10, A) :- !.
+byteToHex(11, B) :- !.
+byteToHex(12, C) :- !.
+byteToHex(13, D) :- !.
+byteToHex(14, E) :- !.
+byteToHex(15, F) :- !.
 
-RGBtoHexAux([H|T], NT) :-
-    isPixRGB(H),
-    RGBtoHexAux(T, NT).
+% Recursión
+byteToHex(Decimal, Hex) :-
+    Decimal > 15,
+    Div is Decimal / 16,
+    Decimal1 is floor(Div),
+    Decimal2 is (Div - Decimal1) * 16,
+    byteToHex(Decimal1, Hex1),
+    byteToHex(Decimal2, Hex2),
+    atom_concat(Hex1, Hex2, Hex).
 
-imageRGBtoHex(Imagen, ImagenH) :-
+% Recursión
+rgbToHex([], []) :- !.
+
+rgbToHex([H|T], [H1|T1]) :-
+    getbyIndex(0, H, R),
+    getbyIndex(1, H, G),
+    getbyIndex(2, H, B),
+    byteToHex(R, R1),
+    byteToHex(G, G1),
+    byteToHex(B, B1),
+    atom_concat(R1, G1, RG),
+    atom_concat(RG, B1, H1),
+    rgbToHex(T, T1).
+
+imageRGBtoHex(Imagen, ImagenHex) :-
     getbyIndex(2, Imagen, Pixs),
-    RGBtoHexAux(Pixs, PixsH),
-    ImagenH = [Altura,Anchura,PixsH].
+    rgbToHex(Pixs, PixsH),
+    ImagenHex = [Altura,Anchura,PixsH].
 
 %------------------------------------------------------------------------------------------------------------------------------
 
